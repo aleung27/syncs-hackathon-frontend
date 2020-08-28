dfc7a6be1ca18cc68668cd76713d50dfa8618f62
adam <adamjleung123@gmail.com>
2020-08-28 23:51:15 +1000

    Started socket things

--- src/Views/Main.js
+++ src/Views/Main.js
@@ -1,11 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+import { Redirect } from "react-router-dom";
 
 const Main = ({ username }) => {
+  const ADDRESS = "http://localhost:8000";
+  const [userPos, setUserPos] = useState(null);
+
   useEffect(() => {
-    console.log(username);
+    const socket = io(ADDRESS);
+
+    socket.on("connect", () => {
+      console.log("connected");
+      socket.send({
+        user: username,
+        "position-x": 0,
+        "position-y": 0,
+      });
+    });
+
+    socket.on("receive", (data) => {
+      setUserPos(data);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
   });
 
-  return <div>Test para</div>;
+  //if (!username) return <Redirect to="./" />;
+
+  return (
+    <div>
+      <canvas
+        id="mainCanvas"
+        width="600"
+        height="600"
+        style={{
+          marginLeft: "50px",
+          marginTop: "50px",
+          border: "1px solid",
+        }}
+      ></canvas>
+    </div>
+  );
 };
 
 export default Main;
